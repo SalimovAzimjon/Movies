1b69820c4eb806c59efbb5702bfb947681a49239
SalimovAzimjon <azim.salimov.99@gmail.com>
2022-06-12 19:33:34 +0500

    movie details screen ready

--- sources/core_api/src/main/java/uz/med/core_api/CoreDependenciesProvider.kt
+++ sources/core_api/src/main/java/uz/med/core_api/CoreDependenciesProvider.kt
@@ -1,5 +1,6 @@
 package uz.med.core_api
 
+import uz.med.core_api.mediator.MediatorProvider
 import uz.med.core_api.service.NetworkServiceProvider
 
-interface CoreDependenciesProvider : AppProvider, MediatorProvider,NetworkServiceProvider
+interface CoreDependenciesProvider : AppProvider, MediatorProvider, NetworkServiceProvider
